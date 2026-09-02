8907036292928a8456d6b3ec06abe446e44612b1
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-02 11:17:36 +0200

    add logback-classic

--- src/test/java/fr/stefangeorgesco/redissonplayground/BaseTests.java
+++ src/test/java/fr/stefangeorgesco/redissonplayground/BaseTests.java
@@ -6,9 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.redisson.api.RedissonClient;
 import org.redisson.api.RedissonReactiveClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class BaseTests {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
     private final RedissonConfig redissonConfig = new RedissonConfig();
     private RedissonClient redissonClient;
