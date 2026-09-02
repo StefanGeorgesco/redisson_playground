bf775f73648a506c0f075e3428dc542bf95a4d26
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2026-09-02 16:03:51 +0200

    add Lec02KeyValueObjectTests

---
+++ src/test/java/fr/stefangeorgesco/redissonplayground/Lec02KeyValueObjectTests.java
@@ -0,0 +1,52 @@
+package fr.stefangeorgesco.redissonplayground;
+
+import fr.stefangeorgesco.redissonplayground.dto.Student;
+import org.junit.jupiter.api.Test;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Lec02KeyValueObjectTests extends BaseTests<Student> {
+
+    @Test
+    void keyValueObjectDefaultCodecTest() {
+        Student student = new Student("Marshal", 20, "Atlanta", List.of(90, 85, 92));
+        bucket = client.getBucket("student:1");
+        verify(student);
+    }
+
+    @Test
+    void keyValueObjectJsonJacksonCodecTest() {
+        Student student = new Student("Marshal", 20, "Atlanta", List.of(90, 85, 92));
+        bucket = client.getBucket("student:1", JsonJacksonCodec.INSTANCE);
+        verify(student);
+    }
+
+    @Test
+    void keyValueObjectTypedJsonJacksonCodecTest() {
+        Student student = new Student("Marshal", 20, "Atlanta", List.of(90, 85, 92));
+        bucket = client.getBucket("student:1", new TypedJsonJacksonCodec(Student.class));
+        verify(student);
+    }
+
+    /*
+        Helper method
+     */
+
+    private void verify(Student student) {
+        set = bucket.set(student);
+        StepVerifier.create(set.then(ttl()).then(get()))
+                .consumeNextWith(s -> {
+                    assertEquals(student.getName(), s.getName());
+                    assertEquals(student.getAge(), s.getAge());
+                    assertEquals(student.getCity(), s.getCity());
+                    assertArrayEquals(student.getMarks().toArray(), s.getMarks().toArray());
+                })
+                .verifyComplete();
+    }
+}
---
+++ src/test/java/fr/stefangeorgesco/redissonplayground/dto/Student.java
@@ -0,0 +1,42 @@
+package fr.stefangeorgesco.redissonplayground.dto;
+
+import java.util.List;
+
+/* Must be a class to work with Redisson's JsonJacksonCodec.INSTANCE codec,
+   but can be a record to work with Redisson's TypedJsonJacksonCodec codec.
+* */
+public class Student {
+
+    private String name;
+    private int age;
+    private String city;
+    private List<Integer> marks;
+
+    @SuppressWarnings("unused")
+    public Student() {
+        // In case of a class, necessary for Redisson to deserialize the object
+    }
+
+    public Student(String name, int age, String city, List<Integer> marks) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public List<Integer> getMarks() {
+        return marks;
+    }
+}
